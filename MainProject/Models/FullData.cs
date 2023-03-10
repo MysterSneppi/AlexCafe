d7c22f2ff93eda1d264df4c0dfbf321b74dd8e8d
Alex <60942937+MysterSneppi@users.noreply.github.com>
2023-03-10 05:10:09 +0200

    Update

---
+++ MainProject/Models/FullData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace MainProject.Models
+{
+    public class FullData
+    {
+        //1 Card 0 No Card
+        public bool MethodOfpayment { get; set; }
+        public double DepositedSum { get; set; }
+        public string? TimeOfPurchase { get; set; }
+
+        public List<ListOfProducts>? ListOfProducts { get; set; }
+
+        public FullData() { }
+
+    }
+
+    public class ListOfProducts
+    {
+        public string? NameProduct { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public ListOfProducts() { }
+        public ListOfProducts(string NameProduct,double Price, int Quantity) 
+        {
+            this.NameProduct = NameProduct;
+            this.Price = Price;
+            this.Quantity = Quantity;
+        }
+    }
+
+}
--- MainProject/ViewModels/MainWindowViewModel.cs
+++ MainProject/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,43 @@
+using MainProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace MainProject.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel() 
+        {
+           List<ListOfProducts> listOfProducts = new List<ListOfProducts>();
+
+            for (int i = 0;i<5;i++) 
+            {
+                ListOfProducts list = new ListOfProducts($"Product{i}",i+100,i);
+                listOfProducts.Add(list);
+            }
+
+            FullData data = new FullData();
+            data.DepositedSum = 200.0;
+            data.TimeOfPurchase = "12:00";
+            data.MethodOfpayment = false;
+            data.ListOfProducts = listOfProducts;
+            
+            
+            string json = JsonConvert.SerializeObject(data);
+
+            Encoding encoding = Encoding.UTF8;
+            File.WriteAllText("C:\\Git\\Vivaldi\\json.txt",json,encoding);
+
+
+        }   
+         
+         
+        
+        
+
+
     }
 }
