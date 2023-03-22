e70286ea13a099be38488279a45aff7dce15b5a2
Alex <60942937+MysterSneppi@users.noreply.github.com>
2023-03-22 10:54:45 +0200

    v.0.2

--- MainProject/Views/MiniDataModul.axaml.cs
+++ MainProject/Views/MiniDataModul.axaml.cs
@@ -1,12 +1,46 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using System.Reflection;
+
 
 namespace MainProject.Views
 {
     public partial class MiniDataModul : UserControl
     {
+
+        public static readonly StyledProperty<string> ImagePathProperty = AvaloniaProperty.Register<MiniDataModul, string>(nameof(ImagePath));
+
+        
+        public string ImagePath 
+        {
+            get { return GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty,value); }
+        }
+
+        public static readonly StyledProperty<string> NameBlockProperty = AvaloniaProperty.Register<MiniDataModul, string>(nameof(NameBlock));
+
+        public string NameBlock
+        {
+            get { return GetValue(NameBlockProperty); }
+            set { SetValue(NameBlockProperty, value); }
+        }
+
+        public static readonly StyledProperty<string> NumberBlockProperty = AvaloniaProperty.Register<MiniDataModul, string>(nameof(NumberBlock));
+
+        public string NumberBlock
+        {
+            get { return GetValue(NumberBlockProperty); }
+            set { SetValue(NumberBlockProperty, value); }
+        }
+
+        
+
         public MiniDataModul()
         {
             InitializeComponent();
+            
+            this.DataContext = this;
         }
     }
 }
