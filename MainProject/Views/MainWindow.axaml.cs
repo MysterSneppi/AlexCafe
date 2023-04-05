3d83eaa8fd673bce51774b7562047bb32edf5abd
Alex <60942937+MysterSneppi@users.noreply.github.com>
2023-04-05 11:12:13 +0300

    1

--- MainProject/Views/MainWindow.axaml.cs
+++ MainProject/Views/MainWindow.axaml.cs
@@ -1,26 +1,13 @@
-using Avalonia.Controls;
-using OxyPlot;
-using System.Collections.Generic;
-using System;
+﻿using Avalonia.Controls;
 
 namespace MainProject.Views
 {
     public partial class MainWindow : Window
     {
-        public IList<DataPoint> MyData { get; }
         public MainWindow()
         {
-            var points = new List<DataPoint>();
-            for (double x = 0; x < 2 * Math.PI; x += 0.1)
-            {
-                double y = Math.Sin(x);
-                points.Add(new DataPoint(x, y));
-            }
-
-            MyData = points;
-
             InitializeComponent();
-            this.DataContext = this;   
         }
     }
 }
+
